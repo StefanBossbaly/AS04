0435e3771b13a826d57818a3d26a18bb94430a79
Stefan Bossbaly <sbossb@gmail.com>
2012-03-23 18:57:25 -0400

    Housekeeping. Added .class files to ignore and changed the build script to the new class files

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-java pre-processor\MacroPP %1 > temp.pep
-java pre-processor\Resolver temp.pep > tempR.pep
-java pre-processor\PrettyPrinter tempR.pep > PEP.pep
+java MacroPP %1 > temp.pep
+java Resolver temp.pep > tempR.pep
+java PrettyPrinter tempR.pep > PEP.pep
 dir PEP.pep
